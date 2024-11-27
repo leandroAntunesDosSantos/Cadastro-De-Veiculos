8d582c09220d8d01a5d3d3fb3a24ad3a909b918c
leandroW <leandro_antunes28@hotmail.com>
2024-11-27 03:45:22 -0300

    metodo post e get de veiculos

--- Dominio/Entidades/Veiculo.cs
+++ Dominio/Entidades/Veiculo.cs
@@ -9,6 +9,12 @@ namespace CadastroVeiculos.Dominio.Entidades
 {
     public class Veiculo
     {
+        public Veiculo(string nome, string marca, int ano)
+        {
+            Nome = nome;
+            Marca = marca;
+            Ano = ano;
+        }
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
